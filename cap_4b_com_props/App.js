140a268d3f1c181d0d209ebbd15db1ad13cd5050
fernando brito <130664535+britoGEMIDA@users.noreply.github.com>
2024-05-16 14:40:53 -0300

    Correção

--- cap_4b_com_props/App.js
+++ cap_4b_com_props/App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import OlaMundo from './components/OlaMundo';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <OlaMundo />
+      <OlaMundo nome='Leitor!'/>
       <StatusBar style="auto" />
     </View>
   );
--- cap_4b_com_props/components/OlaMundo.js
+++ cap_4b_com_props/components/OlaMundo.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Text, View } from "react-native";
 
-export default function OlaMundo() {
+export default function OlaMundo(props) {
     return (
         <View>
             <Text>Olá</Text>
-            <Text>mundo!!</Text>
+            <Text>{props.nome}</Text>
         </View>
     )
 }
