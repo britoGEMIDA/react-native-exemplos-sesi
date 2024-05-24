a4a84477116f3c311752082c4edc40c06485d0ca
fernando brito <130664535+britoGEMIDA@users.noreply.github.com>
2024-05-24 09:53:19 -0300

    comit dos ex

---
+++ cap_11a_firebase/config/config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAX5bXF2m6PUc4O6Ck6BjvXtfVnvbnm5dU",
+  authDomain: "lista-de-linguagens-83c6a.firebaseapp.com",
+  databaseURL: "https://lista-de-linguagens-83c6a-default-rtdb.firebaseio.com",
+  projectId: "lista-de-linguagens-83c6a",
+  storageBucket: "lista-de-linguagens-83c6a.appspot.com",
+  messagingSenderId: "925689325896",
+  appId: "1:925689325896:web:ea4d97ed6ad5c48acbab7d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ cap_12a_hooks/components/Contador.js
@@ -0,0 +1,18 @@
+import { StatusBa } 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Contador from './components/Contador';
+
+export default function App() {
+    return (
+        <View style={StyleSheet.container}>
+        </View>
+        );
+    }
+
+    const styles = StyleSheet.create({
+        container: (
+            flex: 1;
+            backgroundColor
+        )
+    })
